9131032ae513bcdfcc81675b6179917334588bc4
Lucas Gauer <lucas.gauer@reweb.com.br>
2016-06-28 22:05:57 -0300

    start screen

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,1 +1,36 @@
+'use strict';
 
+// Jogo de resta um
+(function () {
+  // var pegSolitaire =
+
+  // Tempo decorrido desde o inicio da partida em segundos
+  var secondsElapsed = 0,
+    timeElapsedEl = document.querySelector('.game-time-elapsed'),
+    timeCounter = function () {
+      secondsElapsed++;
+
+      var time = '',
+        seconds = secondsElapsed,
+        minutes,
+        hours = parseInt(seconds / (60 * 60));
+
+      if (hours) {
+        time += hours + 'h';
+        seconds -= hours * 60 * 60;
+      }
+
+      minutes = parseInt(seconds / 60);
+
+      if (minutes) {
+        time += minutes + 'min';
+        seconds -= minutes * 60;
+      }
+
+      time += seconds + 's';
+
+      timeElapsedEl.innerHTML = time;
+    };
+
+  window.setInterval(timeCounter, 1000);
+})();
